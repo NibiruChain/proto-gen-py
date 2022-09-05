df2610c14fe2c0b5d37d1b275e1cda20db993f52
matthiasmatt <md2022@matrixsystems.co>
2022-09-05 12:34:29 -0300

    fix: add __version__ to nibiru_proto

--- nibiru_proto/__init__.py
+++ nibiru_proto/__init__.py
@@ -1,1 +1,6 @@
+import importlib.metadata as importlib_metadata
 
+try:
+    __version__ = importlib_metadata.version(__package__ or __name__)
+except importlib_metadata.PackageNotFoundError:
+    pass
