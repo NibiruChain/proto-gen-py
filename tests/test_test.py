4ed0cda0453ea88062cd588f504573941103297f
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-05-21 17:45:48 -0500

    fix: import tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -2,18 +2,17 @@ def test_pkg_import():
     import nibiru_proto
 
     print("nibiru_proto directory", dir(nibiru_proto))
-    import nibiru_proto.proto.cosmos.bank
     import nibiru_proto.proto.cosmos.auth
-    import nibiru_proto.proto.cosmos.tx
+    import nibiru_proto.proto.cosmos.bank
     import nibiru_proto.proto.cosmos.gov
     import nibiru_proto.proto.cosmos.staking
-    import nibiru_proto.proto.tendermint
+    import nibiru_proto.proto.cosmos.tx
+    import nibiru_proto.proto.epochs.v1
+    import nibiru_proto.proto.oracle.v1
 
     # imports for Nibi-Perps
-    import nibiru_proto.proto.perp
-    import nibiru_proto.proto.vpool
-    import nibiru_proto.proto.spot
-    import nibiru_proto.proto.epochs
-    import nibiru_proto.proto.oracle
+    import nibiru_proto.proto.perp.v2
+    import nibiru_proto.proto.spot.v1
+    import nibiru_proto.proto.tendermint
 
     print("imports succeeded")
