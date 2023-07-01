587c404b06a0c9b6abc97e8e45cc3bb3915fac12
Unique-Divine <realuniquedivine@gmail.com>
2023-07-01 15:42:09 -0500

    release,test: v0.21.3b2: increase test coverage for new dependencies + publish

--- tests/test_test.py
+++ tests/test_test.py
@@ -2,12 +2,6 @@ def test_pkg_import():
     import nibiru_proto
 
     print("nibiru_proto directory", dir(nibiru_proto))
-    import nibiru_proto.cosmos.bank
-    import nibiru_proto.cosmos.auth
-    import nibiru_proto.cosmos.tx
-    import nibiru_proto.cosmos.gov
-    import nibiru_proto.cosmos.staking
-    import nibiru_proto.tendermint
 
     # imports for Nibi-Perps
     import nibiru_proto.nibiru.epochs
@@ -19,3 +13,24 @@ def test_pkg_import():
     import nibiru_proto.nibiru.sudo
 
     print("imports succeeded")
+
+
+def test_cosmos_imports():
+    import nibiru_proto.cosmos.bank
+    import nibiru_proto.cosmos.auth
+    import nibiru_proto.cosmos.tx
+    import nibiru_proto.cosmos.gov
+    import nibiru_proto.cosmos.staking
+    import nibiru_proto.tendermint
+
+
+def test_cosmos_subdir_imports():
+    from nibiru_proto.cosmos.tx.v1beta1 import tx_pb2
+    from nibiru_proto.cosmos.tx.v1beta1 import service_pb2
+    from nibiru_proto.cosmos.base.tendermint.v1beta1 import (
+        query_pb2 as tendermint_query,
+    )
+
+    from nibiru_proto.cosmos.bank.v1beta1 import tx_pb2
+    from nibiru_proto.cosmos.distribution.v1beta1 import tx_pb2
+    from nibiru_proto.cosmos.distribution.v1beta1 import distribution_pb2
